424672c231d71be891e190447c6782a8d3ff9ab4
tbouete <titouan.bouete@gmail.com>
2017-10-20 15:21:19 +0200

    TestEstVert + TestEstBleu

--- src/JaugeTest/JaugeNaturelTest.java
+++ src/JaugeTest/JaugeNaturelTest.java
@@ -1,5 +1,29 @@
 package JaugeTest;
 
-public class JaugeNaturelTest {
+import org.junit.Test;
 
+public class JaugeNaturelTest {
+	
+	JaugeNaturel jaugeVerte = new JaugeNaturel(0, 5, 2);
+	JaugeNaturel jaugeRouge = new JaugeNaturel(0, 5, 6);
+	JaugeNaturel jaugeBleue = new JaugeNaturel(0, 5, -2);
+	
+	@Test
+	public void testEstVert(){
+		assert jaugeVerte.estVert() : "ma jaugeVerte doit être verte";
+		assert !jaugeRouge.estVert() : "ma jaugeRouge ne doit pas être verte ";
+		assert !jaugeBleue.estVert() : "ma jaugeBleue ne doit pas être verte ";
+		assert !new JaugeNaturel(0, 5, 0).estVert() : "une jauge à la limite inférieure ne doit pas être verte";
+		assert !new JaugeNaturel(0, 5, 5).estVert() : "une jauge à la limite suppérieure ne doit pas être verte";
+	}
+	
+	@Test
+	public void testEstBleu(){
+		assert !jaugeVerte.estBleu() : "ma jaugeVerte ne doit pas être bleue";
+		assert !jaugeRouge.estBleu() : "ma jaugeRouge ne doit pas être bleue ";
+		assert jaugeBleue.estBleu() : "ma jaugeBleue doit être bleue ";
+		assert new JaugeNaturel(0, 5, 0).estBleu() : "une jauge à la limite inférieure doit être bleue";
+		assert !new JaugeNaturel(0, 5, 5).estBleu() : "une jauge à la limite suppérieure ne doit pas être bleue";
+	}
+	
 }
