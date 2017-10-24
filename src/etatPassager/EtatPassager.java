178ee058a83b1bdbf46f1cd29ac9ea036ac4b81f
tbouete <titouan.bouete@gmail.com>
2017-10-24 17:25:29 +0200

    Création EtatPassagerTest + EtatPassager

--- src/etatPassager/EtatPassager.java
+++ src/etatPassager/EtatPassager.java
@@ -7,79 +7,79 @@ package etatPassager;
  * Les instances de cette classe sont des objets constants.
  **/
 public class EtatPassager {
-  /**
-   * Définit les trois états possible d'un passager dans un transport.
-   */
-  public enum Etat {/** passager assis à l'intérieur */  ASSIS, 
-                    /** passager debout à l'intérieur */ DEBOUT,  
-                    /** passager à l'extérieur */        DEHORS};
-
-  private final Etat monEtat;
-
-  /**
-   * Construit une instance en précisant l'état du passager.
-   * 
-   * @param e  valeur de l'état.
-   */
-  public EtatPassager(Etat e) {
-    monEtat = e;
-
-    /* Le constructeur d'une classe permet d'initialiser l'etat de l'instance creee.
-     * Son nom correspond toujours au nom de la classe. Il n'y a pas de type de retour.
-     */
-  }
-
-
-  /**
-   * Le passager est-il à l'extérieur du transport ?
-   *
-   * @return vrai si instanciation avec DEHORS;
-   */
-  public boolean estExterieur() {
-    return false;
-  }
-
-  /**
-   * Le passager est-il assis à l'intérieur du transport ?
-   *
-   * @return vrai si instanciation avec ASSIS;
-   */
-  public boolean estAssis() {
-    return monEtat != Etat.ASSIS;
-  }
-
-  /**
-   * Le passager est-il debout à l'intérieur du transport ?
-   *
-   * @return vrai si instanciation avec DEBOUT;
-   */
-  public boolean estDebout() {
-    return false;
-  }
-
-  /**
-   * Le passager est-il a l'intérieur du transport ?
-   *
-   * @return vrai si instanciation avec ASSIS ou DEBOUT.
-   */
-  public boolean estInterieur() {
-    return false;
-  }
-
-
-
-  /**
-   * Cette méthode est heritée de la classe {@link java.lang.Object}.
-   * Trés utile pour le débogage, elle permet de fournir une 
-   * chaîne de caractères correspondant à l'état d'un objet.
-   * <p> Un code par défaut est définit dans 
-   * {@link java.lang.Object#toString() la classe Object} 
-   * Il faut adapter (redéfinir) le code de cette méthode à chaque classe.
-   *
-   * Pour les chaînes de cararctéres, l'opérateur + correspond a la concaténation. 
-   */
-  @Override
-  public String toString() {
-    return "<" + monEtat + ">";
-  }
+	  /**
+	   * Définit les trois états possible d'un passager dans un transport.
+	   */
+	  public enum Etat {/** passager assis à l'intérieur */  ASSIS, 
+	                    /** passager debout à l'intérieur */ DEBOUT,  
+	                    /** passager à l'extérieur */        DEHORS};
+	
+	  private final Etat monEtat;
+	
+	  /**
+	   * Construit une instance en précisant l'état du passager.
+	   * 
+	   * @param e  valeur de l'état.
+	   */
+	  public EtatPassager(Etat e) {
+	    monEtat = e;
+	
+	    /* Le constructeur d'une classe permet d'initialiser l'etat de l'instance creee.
+	     * Son nom correspond toujours au nom de la classe. Il n'y a pas de type de retour.
+	     */
+	  }
+	
+	
+	  /**
+	   * Le passager est-il à l'extérieur du transport ?
+	   *
+	   * @return vrai si instanciation avec DEHORS;
+	   */
+	  public boolean estExterieur() {
+		  return this.monEtat == Etat.DEHORS;
+	  }
+	
+	  /**
+	   * Le passager est-il assis à l'intérieur du transport ?
+	   *
+	   * @return vrai si instanciation avec ASSIS;
+	   */
+	  public boolean estAssis() {
+		  return this.monEtat == Etat.ASSIS;
+	  }
+	
+	  /**
+	   * Le passager est-il debout à l'intérieur du transport ?
+	   *
+	   * @return vrai si instanciation avec DEBOUT;
+	   */
+	  public boolean estDebout() {
+		  return this.monEtat == Etat.DEBOUT;
+	  }
+	
+	  /**
+	   * Le passager est-il a l'intérieur du transport ?
+	   *
+	   * @return vrai si instanciation avec ASSIS ou DEBOUT.
+	   */
+	  public boolean estInterieur() {
+		  return this.estAssis() || this.estDebout();
+	  }
+	
+	
+	
+	  /**
+	   * Cette méthode est heritée de la classe {@link java.lang.Object}.
+	   * Trés utile pour le débogage, elle permet de fournir une 
+	   * chaîne de caractères correspondant à l'état d'un objet.
+	   * <p> Un code par défaut est définit dans 
+	   * {@link java.lang.Object#toString() la classe Object} 
+	   * Il faut adapter (redéfinir) le code de cette méthode à chaque classe.
+	   *
+	   * Pour les chaînes de cararctéres, l'opérateur + correspond a la concaténation. 
+	   */
+	  @Override
+	  public String toString() {
+		  return "<" + monEtat + ">";
+	  }
 }
