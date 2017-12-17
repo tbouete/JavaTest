6533016d2c19512a57e756ad3d4589cdae7eac52
mdachez <maelle.d46@gmail.com>
2017-12-17 22:35:43 +0100

    PassagerStandard terminé, debut Autobus

--- src/tec/Autobus.java
+++ src/tec/Autobus.java
@@ -1,9 +1,118 @@
 package tec;
 
-public class Autobus {
+public class Autobus implements Transport, Bus
+{
 
 	public Autobus() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void allerArretSuivant() throws UsagerInvalideException
+	{
+		
+	}
+	
+	
+	  /**
+	   * vrai s'il existe des places assises.
+	   * @return vrai s'il existe des places assises
+	   */
+	  public boolean aPlaceAssise()
+	  {
+		  boolean wesh = false;
+		  return false;
+	  }
+
+	  
+	  
+	  
+	  
+	  /**
+	   * vrai s'il existe des places debouts.
+	   * @return vrai s'il existe des places debouts
+	   */
+	  public boolean aPlaceDebout()
+	  { 
+		  boolean wesh = false;
+		  return false;
+		  
+	  }
+
+	  
+	  
+	  
+	  
+	  /**
+	   * Le passager entre dans ce bus en demandant une place assise.
+	   * L'état du passager est forcément dehors.
+	   * Cette méthode est appelée par Passager.
+	   * @param p le passager
+	   */
+	  public void demanderPlaceAssise(PassagerStandard p)
+	  {
+		  
+	  }
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  /**
+	   * Le passager entre dans ce bus en demandant une place debout.
+	   * L'état du passager est forcément dehors.
+	   * Cette méthode est appelée par passager. 
+	   * Elle change l'état du passager.
+	   * @param p le passager
+	   */
+	  public void demanderPlaceDebout(Passager p)
+	  {
+		  
+	  }
+
+
+	  
+	  
+	  
+	  
+	  /**
+	   * Change un passager d'une place assise vers une place debout.
+	   * Elle change l'état du passager.
+	   * @param p le passager avec un état assis.
+	   */
+	  public void demanderChangerEnDebout(Passager p)
+	  {
+		  
+	  }
+
+	  
+	  
+	  
+	  
+	  
+	  /**
+	   * Change un passager d'une place debout vers une place assise.
+	   * Elle change l'état du passager.
+	   * @param p le passager avec un état debout.
+	   */
+	  public void demanderChangerEnAssis(Passager p)
+	  {
+		  
+	  }
+
+	  
+	  
+	  
+	  /**
+	   * Fait sortir un passager du bus.
+	   * Elle change l'état du passager.
+	   * @param p le passager avec un état soit assis soit debout.
+	   */
+	  public void demanderSortie(Passager p)
+	  {
+		  
+	  }
+	  
+	  
 }
--- src/tec/PassagerStandard.java
+++ src/tec/PassagerStandard.java
@@ -1,29 +1,152 @@
 package tec;
 
-public class PassagerStandard {
+import etatPassager.EtatPassager;
+import etatPassager.EtatPassager.Etat;
+
+public class PassagerStandard implements Passager, Usager
+{
 	
-	private String nomPassager;
-	private int destination;
+	public String nom;
+	public int destination;
+	public EtatPassager monEtat;
 	
 
-	public PassagerStandard(String nom, int numeroArret)
+	public PassagerStandard(String nomPassager, int numeroArret)
 	{
-		this.nomPassager = nom;
+		Etat e = etatPassager.EtatPassager.Etat.DEHORS;
+		this.nom = nomPassager;
 		this.destination = numeroArret;
+		this.monEtat = new EtatPassager(e);
 	}
-	
-	  //Accesseurs
-	  public String getNomPassager() {return nomPassager;}
-	  public void setNomPassager(String nomPassager) {this.nomPassager = nomPassager;}
-	  public int getDestination() {return destination;}
-	  public void setDestination(int destination) {this.destination = destination;}
-
-	  public void monterDans(Autobus autobus)
+		  
+	  
+	  public String nom()
 	  {
+		  return this.nom;
+	  }
+	  
+	  
+	  
+	  /**
+	   * Retourne vrai si le passager est hors du bus.
+	   * @return vrai si le passager est hors du bus.
+	   */
+	  public boolean estDehors()
+	  {
+		  boolean etat = this.monEtat.estExterieur();
+		  return etat ;
+		  
+	  }	  
+	  
+	
+	  
+	  /**
+	   * Retourne vrai si le passager est assis dans le bus.
+	   * @return vrai si le passager est assis dans le bus.
+	   */
+	  public boolean estAssis()
+	  {
+		  boolean etat = this.monEtat.estAssis();
+		  return etat ;
+	  }
+	    
+	  
+	  
+	  /**
+	   * Retourne vrai si le passager est debout dans le bus.
+	   * @return vrai si le passager est debout dans le bus.
+	   */
+	  public boolean estDebout()
+	  { 
+		  boolean etat = this.monEtat.estDebout();
+		  return etat ;
 		  
 	  }
+	  
+	  
+	  
+	  /**
+	   * Change l'�tat du passager en hors du bus.
+	   * Cette m�thode est appel�e par un objet Bus.
+	   */
+	  public void accepterSortie()
+	  {
+		  this.monEtat = new EtatPassager(etatPassager.EtatPassager.Etat.DEHORS);
+	  }
+	  
+	  
+	  
+	  
+	  /**
+	   * Change l'état du passager en assis. 
+	   * Le passager est dans le bus.
+	   * Cette méthode est appelée par un objet Bus.
+	   */
+	  public void accepterPlaceAssise()
+	  {
+		  this.monEtat = new EtatPassager(etatPassager.EtatPassager.Etat.ASSIS);
+	  }
+	  
+	  
+	  
+	  
+	  /**
+	   * Change l'état du passager en debout.
+	   * Le passager est dans le bus.
+	   * Cette méthode est appelée par un objet Bus.
+	   */
+	  public void accepterPlaceDebout()
+	  {
+		  this.monEtat = new EtatPassager(etatPassager.EtatPassager.Etat.DEBOUT);		  
+		  
+	  }
+
+	  
+	  
+	  /**
+	   * Indique au passager qu'il est arrivé à un nouvel arrêt. Cette methode
+	   * fixe le comportement (changer de place ou sortir). 
+	   * Cette méthode est appelée par Bus.
+	   *
+	   * @param bus le bus dans lequel se trouve le passager.
+	   * @param numeroArret numero de l'arrêt.
+	   */
+	  public void nouvelArret(Bus bus, int numeroArret)
+	  {
+		  if (numeroArret == this.destination)
+		  {
+			  ((Autobus)bus).demanderSortie(this);
+		  }		  
+	  }
+
+	  
+	  
+	  
+	  /**
+	   * Fournit à l'usager le transport auquel il peut accéder.
+	   * Cette méthode réalise le caractère du passager à la montée.
+	   * 
+	   * @param t le transport dans lequel désire monter l'usager.
+	   * @throws si l'état de l'usager est incohérent par rapport à sa demande.
+	   */
+
+	  public void monterDans(Transport t) throws UsagerInvalideException
+	  {
+		  if (t instanceof Autobus)
+		  {
+			  ((Autobus)t).demanderPlaceDebout(this);
+			  if (this.estDehors())
+			  {
+				  ((Autobus)t).demanderPlaceAssise(this);
+			  }
+			  else if(this.estDehors())
+			 {
+				  throw new UsagerInvalideException ("Il n'y a plus de place disponible.");
+			 }
+		  }
+		  
+	  }
+
 	
-
-
 
 }
