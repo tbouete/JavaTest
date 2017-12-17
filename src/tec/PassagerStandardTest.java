2098c6ce77c0e2604a6596b1cb037048e2d11d06
mdachez <maelle.d46@gmail.com>
2017-12-17 13:48:54 +0100

    Création test Autobus+PassagerStandard, modif interface Transport

---
+++ src/tec/PassagerStandardTest.java
@@ -0,0 +1,14 @@
+package tec;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PassagerStandardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
