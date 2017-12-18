5312061e94727461904760f7f1b7334e8a01018e
mdachez <maelle.d46@gmail.com>
2017-12-18 09:15:52 +0100

    ajout saut de ligne EtatPassager CaSertARienHiHiHi

--- src/etatPassager/EtatPassager.java
+++ src/etatPassager/EtatPassager.java
@@ -6,7 +6,8 @@ package etatPassager;
  *  
  * Les instances de cette classe sont des objets constants.
  **/
-public class EtatPassager implements IEtatPassager{
+public class EtatPassager implements IEtatPassager
+{
 	  /**
 	   * Définit les trois états possible d'un passager dans un transport.
 	   */
@@ -16,12 +17,15 @@ public class EtatPassager implements IEtatPassager{
 	
 	  private final Etat monEtat;
 	
+	  
+	  
 	  /**
 	   * Construit une instance en précisant l'état du passager.
 	   * 
 	   * @param e  valeur de l'état.
 	   */
-	  public EtatPassager(Etat e){
+	  public EtatPassager(Etat e)
+	  {
 	    monEtat = e;
 	
 	    /* Le constructeur d'une classe permet d'initialiser l'etat de l'instance creee.
@@ -30,42 +34,59 @@ public class EtatPassager implements IEtatPassager{
 	  }
 	
 	
+	  
+	  
 	  /**
 	   * Le passager est-il à l'extérieur du transport ?
 	   *
 	   * @return vrai si instanciation avec DEHORS;
 	   */
-	  public boolean estExterieur() {
+	  public boolean estExterieur() 
+	  {
 		  return this.monEtat == Etat.DEHORS;
 	  }
 	
+	  
+	  
+	  
 	  /**
 	   * Le passager est-il assis à l'intérieur du transport ?
 	   *
 	   * @return vrai si instanciation avec ASSIS;
 	   */
-	  public boolean estAssis() {
+	  public boolean estAssis() 
+	  {
 		  return this.monEtat == Etat.ASSIS;
 	  }
+	  
+	  
+	  
 	
 	  /**
 	   * Le passager est-il debout à l'intérieur du transport ?
 	   *
 	   * @return vrai si instanciation avec DEBOUT;
 	   */
-	  public boolean estDebout() {
+	  public boolean estDebout() 
+	  {
 		  return this.monEtat == Etat.DEBOUT;
 	  }
 	
+	  
+	  
+	  
 	  /**
 	   * Le passager est-il a l'intérieur du transport ?
 	   *
 	   * @return vrai si instanciation avec ASSIS ou DEBOUT.
 	   */
-	  public boolean estInterieur() {
+	  public boolean estInterieur() 
+	  {
 		  return this.estAssis() || this.estDebout();
 	  }
 	
+	  
+	  
 	
 	
 	  /**
@@ -79,7 +100,8 @@ public class EtatPassager implements IEtatPassager{
 	   * Pour les chaînes de cararctéres, l'opérateur + correspond a la concaténation. 
 	   */
 	  @Override
-	  public String toString() {
+	  public String toString() 
+	  {
 		  return "<" + monEtat + ">";
 	  }
 }
