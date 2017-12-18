6cf99a46bb64c51d6e0819454c9e4cde03186f98
mdachez <maelle.d46@gmail.com>
2017-12-18 14:13:20 +0100

    PassagerStandard presque terminéeeeee euh

--- src/tec/PassagerStandardTest.java
+++ src/tec/PassagerStandardTest.java
@@ -8,108 +8,137 @@ import etatPassager.EtatPassager;
 
 public class PassagerStandardTest 
 {
-	
+
 	PassagerStandard Kaylee = new PassagerStandard("Kaylee", 5);
 	PassagerStandard Jayne = new PassagerStandard("Jayne", 4);
 	PassagerStandard Inara = new PassagerStandard("Inara", 5);
-	
-	
-	
-	
-	  @Test
-	  public void testNom()
-	  {
-		  assertEquals("Kaylee",Kaylee.nom());
-		  assertFalse("Jayne".equals(Kaylee.nom()));
-	  }
-	  
-	  
-	  
-	  @Test
-	  public void testEstDehors()
-	  {
-		 Jayne.accepterPlaceAssise();
-		 assertTrue(Kaylee.estDehors());
-		 assertFalse(Jayne.estDehors());
-	  }	  
-	  
-	
-	  
-	  @Test
-	  public void testEstAssis()
-	  {
-		  Jayne.accepterPlaceAssise();
-		  assertFalse(Kaylee.estAssis());
-		  assertTrue(Jayne.estAssis());
-	  }
-	    
-	  
-	  
-	  @Test
-	  public void testEstDebout()
-	  {
-		  Jayne.accepterPlaceDebout();
-		  assertFalse(Kaylee.estAssis());
-		  assertTrue(Jayne.estAssis());
-	  }
-	  
-//	  
-//	  
-//	  @Test
-//	  public void testAccepterSortie()
-//	  {
-//		  this.monEtat = new EtatPassager(etatPassager.EtatPassager.Etat.DEHORS);
-//	  }
-//	  
-//	  
-//	  
-//	  
-//	  @Test
-//	  public void testAccepterPlaceAssise()
-//	  {
-//		  this.monEtat = new EtatPassager(etatPassager.EtatPassager.Etat.ASSIS);
-//	  }
-//	  
-//	  
-//	  
-//	  
-//	  @Test
-//	  public void testAccepterPlaceDebout()
-//	  {
-//		  this.monEtat = new EtatPassager(etatPassager.EtatPassager.Etat.DEBOUT);		  
-//		  
-//	  }
-//
-//	
-//	
-//	
-//	@Test
-//	  public void testNouvelArret(Bus bus, int numeroArret)
-//	  {
-//		  if (numeroArret == this.destination)
-//		  {
-//			  ((Autobus)bus).demanderSortie(this);
-//		  }		  
-//	  }
-//
-//	  
-//	  
-//	  
-//
-//		@Test
-//	  public void testMonterDans(Transport t) throws UsagerInvalideException
-//	  {
-//		  if (t instanceof Autobus)
-//		  {
-//			  ((Autobus)t).demanderPlaceDebout(this);
-//			  if (this.estDehors())
-//			  {
-//				  ((Autobus)t).demanderPlaceAssise(this);
-//			  }
-//			  else if(this.estDehors())
-//			 {
-//				  throw new UsagerInvalideException ("Il n'y a plus de place disponible.");
-//			 }
-//		  }
+
+
+
+
+	@Test
+	public void testNom()
+	{
+		assertEquals("Kaylee",Kaylee.nom());
+		assertFalse("Jayne".equals(Kaylee.nom()));
+	}
+
+
+
+	@Test
+	public void testEstDehors()
+	{
+		Jayne.accepterPlaceAssise();
+		Inara.accepterPlaceDebout();
+
+		assertTrue(Kaylee.estDehors());
+		assertFalse(Jayne.estDehors());
+		assertFalse(Inara.estDehors());
+	}	  
+
+
+
+	@Test
+	public void testEstAssis()
+	{
+		Jayne.accepterPlaceAssise();
+		Inara.accepterPlaceDebout();
+
+		assertFalse(Kaylee.estAssis());
+		assertTrue(Jayne.estAssis());
+		assertFalse(Inara.estAssis());
+
+	}
+
+
+
+	@Test
+	public void testEstDebout()
+	{
+		Jayne.accepterPlaceAssise();
+		Inara.accepterPlaceDebout();
+
+		assertFalse(Kaylee.estDebout());
+		assertFalse(Jayne.estDebout());
+		assertTrue(Inara.estDebout());
+	}
+
+
+
+	@Test
+	public void testAccepterSortie()
+	{
+		Kaylee.accepterPlaceAssise();
+		Jayne.accepterSortie();
+		Inara.accepterPlaceDebout();
+
+		assertFalse(Kaylee.estDehors());
+		assertTrue(Jayne.estDehors());
+		assertFalse(Inara.estDehors());
+
+	}
+
+
+
+
+	@Test
+	public void testAccepterPlaceAssise()
+	{
+		Kaylee.accepterSortie();
+		Jayne.accepterPlaceAssise();
+		Inara.accepterPlaceDebout();
+
+		assertFalse(Kaylee.estAssis());
+		assertTrue(Jayne.estAssis());
+		assertFalse(Inara.estAssis());
+	}
+
+
+
+
+	@Test
+	public void testAccepterPlaceDebout()
+	{
+		Kaylee.accepterSortie();
+		Jayne.accepterPlaceAssise();
+		Inara.accepterPlaceDebout();
+
+		assertFalse(Kaylee.estDebout());
+		assertFalse(Jayne.estDebout());
+		assertTrue(Inara.estDebout());	  
+
+	}
+
+
+
+
+	//	@Test
+	//	  public void testNouvelArret(Bus bus, int numeroArret)
+	//	  {
+	//		  if (numeroArret == this.destination)
+	//		  {
+	//			  ((Autobus)bus).demanderSortie(this);
+	//		  }		  
+	//	  }
+
+	//	  
+	//	  
+	//	  
+	//
+	//		@Test
+	//	  public void testMonterDans(Transport t) throws UsagerInvalideException
+	//	  {
+	//		  if (t instanceof Autobus)
+	//		  {
+	//			  ((Autobus)t).demanderPlaceDebout(this);
+	//			  if (this.estDehors())
+	//			  {
+	//				  ((Autobus)t).demanderPlaceAssise(this);
+	//			  }
+	//			  else if(this.estDehors())
+	//			 {
+	//				  throw new UsagerInvalideException ("Il n'y a plus de place disponible.");
+	//			 }
+	//		  }
 
 }
