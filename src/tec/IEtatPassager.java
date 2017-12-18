671ce58afa458c125fe04e4aa6fc608960016577
mdachez <maelle.d46@gmail.com>
2017-12-18 15:37:18 +0100

    Problème

---
+++ src/tec/IEtatPassager.java
@@ -0,0 +1,10 @@
+package tec;
+
+public interface IEtatPassager {
+	
+	public boolean estInterieur();
+	public boolean estExterieur();
+	public boolean estAssis();
+	public boolean estDebout();
+
+}
